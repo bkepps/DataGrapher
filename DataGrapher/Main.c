8621a212988e7b41b39bddea2879bfa6f608bf6d
bkepps <59382763+bkepps@users.noreply.github.com>
2020-06-29 10:36:06 -0500

    now with multiple threads!

--- DataGrapher/Main.c
+++ DataGrapher/Main.c
@@ -1,33 +1,28 @@
-#include <SDL.h>
-#include <stdio.h>
-#include <Windows.h>
+
+
+#include "MainHead.h"
 
 int main() {
 	int i = 0;
 	float j;
-	const char* title = "Data Plotter V1";
+	const char* title = "Data Plotter V2";
 	Uint32 width = 380;
 	Uint32 height = 700;
 	Uint32 graphSizew = 380;
 	Uint32 graphSizeh = 600;
 	Uint8 quit = 0;			//BOOL
-	Uint8 charRead = 1;		//BOOL, 0 when no chars left to read
-	Uint8 end = 0;			//BOOL
 	SDL_Event event;
-	char* file_rBuf = malloc(sizeof(char));			//BUFFER, for reading one char from a file
-	char* data_Buf = malloc(sizeof(char) * 5);		//BUFFER
-	Uint32 dataNumr = 0;							//counts data number being read
 	//FILE* data;				//data stored as chars terminated with \r, must have \r at end of last line
-	SDL_Point* points = malloc(sizeof(SDL_Point) * 1000);
 	int datapoints[1000];
 
-	HANDLE hComm;							// Handle to the Serial port
-	char  ComPortName[] = "\\\\.\\COM4";	// Name of the Serial port(May Change) to be opened,
-	BOOL  Status;							// Status of the various operations 
 	DWORD dwEventMask;						// Event mask to trigger
-	char  TempChar;							// Temperory Character
 	char  SerialBuffer[256];				// Buffer Containing Rxed Data
-	DWORD NoBytesRead;						// Bytes read by ReadFile()
+
+	gather_data* data = malloc(sizeof(gather_data));
+	data->numOfPoints = 100;
+	data->points = malloc(sizeof(SDL_Point) * data->numOfPoints);
+	data->updated = 0;
+	data->run = 0;
 
 //#pragma warning(suppress : 4996)							// lets me use the unsafe version of fread()
 	//data = fopen("C:\\Users\\bensk\\data.txt", "r");		//open file with points to read
@@ -60,34 +55,27 @@ int main() {
 	//free(charRead);
 	//free(data);
 
-	/*open serial port*/
-	hComm = CreateFile(L"COM4",                  // Name of the Port to be Opened
-		GENERIC_READ | GENERIC_WRITE, // Read/Write Access
+	data->port = CreateFile(L"COM4",                  // Name of the Port to be Opened
+		GENERIC_READ,							// Read Access
 		0,                            // No Sharing, ports cant be shared
 		NULL,                         // No Security
 		OPEN_EXISTING,                // Open existing port only
 		0,                            // Non Overlapped I/O
 		NULL);                        // Null for Comm Devices
 
-	if (hComm == INVALID_HANDLE_VALUE)
+	if (data->port == INVALID_HANDLE_VALUE)
 		return 1;
 	DCB dcbSerialParams = { 0 };                         // Initializing DCB structure
 	dcbSerialParams.DCBlength = sizeof(dcbSerialParams);
 
-	Status = GetCommState(hComm, &dcbSerialParams);      //retreives  the current settings
-
-	if (Status == FALSE)
-		return 2;
+	GetCommState(data->port, &dcbSerialParams);      //retreives  the current settings
 
 	dcbSerialParams.BaudRate = CBR_9600;      // Setting BaudRate = 9600
 	dcbSerialParams.ByteSize = 8;             // Setting ByteSize = 8
 	dcbSerialParams.StopBits = ONESTOPBIT;    // Setting StopBits = 1
 	dcbSerialParams.Parity = NOPARITY;        // Setting Parity = None 
 
-	Status = SetCommState(hComm, &dcbSerialParams);  //Configuring the port according to settings in DCB 
-
-	if (Status == FALSE)
-	return 3;
+	SetCommState(data->port, &dcbSerialParams);  //Configuring the port according to settings in DCB 
 
 	COMMTIMEOUTS timeouts = { 0 };
 	timeouts.ReadIntervalTimeout = 50;
@@ -96,7 +84,7 @@ int main() {
 	timeouts.WriteTotalTimeoutConstant = 50;
 	timeouts.WriteTotalTimeoutMultiplier = 10;
 
-	if (SetCommTimeouts(hComm, &timeouts) == FALSE)
+	if (SetCommTimeouts(data->port, &timeouts) == FALSE)
 		return 4;
 
 	while (!quit) {
@@ -120,55 +108,44 @@ int main() {
 			
 		}
 
-		do {
-			i = 0;
-			end = 0;
-			/*step through chars and append to data_rBuf until a return char is found. return char is last char in string*/
-			do {
-				ReadFile(hComm, file_rBuf, sizeof(TempChar), &NoBytesRead, NULL);		//returns 0 when no chars left to read, will store one more point, we'll ignore it later
-				if (*file_rBuf == '\r')
-					end = 1;
-				data_Buf[i++] = *file_rBuf;
-			} while (!end);
-			/*convert data_buf to int. atoi ignores \r at end of string*/
-			if (!NoBytesRead)
-				break;
-			//datapoints[dataNumr] = atoi(data_Buf);
-			points[dataNumr].y = graphSizeh - (atoi(data_Buf) / 2);
-			points[dataNumr].x = ++dataNumr;
-		} while (NoBytesRead);
+		if (data->updated) {
 
-		/*render everything*/
-		SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
-		SDL_RenderClear(ren);
-		/*set color for grid*/
-		SDL_SetRenderDrawColor(ren, 200, 200, 200, 255);
-		/*creates horizontal lines about 50mV apart*/
-		for (j = graphSizeh; j >= 0; j -= 10.23) {
-			SDL_RenderDrawLine(ren, 0, j, (dataNumr - 1), j);
+			/*render everything*/
+			SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
+			SDL_RenderClear(ren);
+			/*set color for grid*/
+			SDL_SetRenderDrawColor(ren, 200, 200, 200, 255);
+			/*creates horizontal lines about 50mV apart*/
+			for (j = graphSizeh; j >= 0; j -= 10.23) {
+				SDL_RenderDrawLine(ren, 0, j, (data->numOfPoints - 1), j);
+			}
+			/*creates vertical lines 12 points (at 10 seconds per point, 2 minutes per line) apart*/
+			for (i = 0; i <= (data->numOfPoints - 1); i += 12) {
+				SDL_RenderDrawLine(ren, i, 0, i, graphSizeh);
+			}
+			/*creates red vertical lines every 10 minutes at 6 samples per second*/
+			SDL_SetRenderDrawColor(ren, 150, 150, 150, 255);
+			for (i = 0; i <= (data->numOfPoints - 1); i += 60) {
+				SDL_RenderDrawLine(ren, i, 0, i, graphSizeh);
+			}
+			/*creates horizontal red line every 1V with 10-bit 0-5V adc*/
+			for (j = graphSizeh; j >= 0; j -= 204.6) {
+				SDL_RenderDrawLine(ren, 0, j, (data->numOfPoints - 1), j);
+			}
+			/*set the color of the points*/
+			SDL_SetRenderDrawColor(ren, 0, 0, 255, 255);
+			/*plot data points and connect them, but not last one since it's garbo*/
+			SDL_RenderDrawLines(ren, data->points, (data->numOfPoints - 1));
+			/*put the render color back to white*/
+			data->updated = 0;
 		}
-		/*creates vertical lines 12 points (at 10 seconds per point, 2 minutes per line) apart*/
-		for (i = 0; i <= (dataNumr - 1); i += 12) {
-			SDL_RenderDrawLine(ren, i, 0, i, graphSizeh);
+		if(!data->run) {
+			_beginthread(Gather, 0, data);
+			data->run = 1;
 		}
-		/*creates red vertical lines every 10 minutes at 6 samples per second*/
-		SDL_SetRenderDrawColor(ren, 150, 150, 150, 255);
-		for (i = 0; i <= (dataNumr - 1); i += 60) {
-			SDL_RenderDrawLine(ren, i, 0, i, graphSizeh);
-		}
-		/*creates horizontal red line every 1V with 10-bit 0-5V adc*/
-		for (j = graphSizeh; j >= 0; j -= 204.6) {
-			SDL_RenderDrawLine(ren, 0, j, (dataNumr - 1), j);
-		}
-		/*set the color of the points*/
-		SDL_SetRenderDrawColor(ren, 0, 0, 255, 255);
-		/*plot data points and connect them, but not last one since it's garbo*/
-		SDL_RenderDrawLines(ren, points, (dataNumr - 1));
-		/*put the render color back to white*/
-
 		SDL_RenderPresent(ren);
 	}
 	SDL_Quit();
-	CloseHandle(hComm);//Closing the Serial Port
+	CloseHandle(data->port);//Closing the Serial Port
 	return 0;
 }
